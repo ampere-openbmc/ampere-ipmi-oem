263b59a8ed1a2a2a1f5084c5226a95d58c7fcd16
Quan Nguyen <qnguyen@amperecomputing.com>
2022-03-04 11:00:14 +0700

    Update new smpro child driver paths
    Update SMpro /sys path to match the new change in smpro-hwmon,
    smpro-errmon and smpro-misc drivers.

--- src/oemcommands.cpp
+++ src/oemcommands.cpp
@@ -34,8 +34,8 @@ using FruObjectType = boost::container::flat_map<
         boost::container::flat_map<std::string, BasicVariantType>>>;
 
 static std::vector<std::string> scpRWPath =  {
-    "/sys/bus/i2c/devices/2-004f/1e78a0c0.i2c-bus:smpro@4f:misc/",
-    "/sys/bus/i2c/devices/2-004e/1e78a0c0.i2c-bus:smpro@4e:misc/"
+    "/sys/bus/platform/devices/smpro-misc.2.auto/",
+    "/sys/bus/platform/devices/smpro-misc.5.auto/",
 };
 
 constexpr static const char* fruDeviceServiceName =
